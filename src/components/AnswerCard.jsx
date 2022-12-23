c31b9ac0605f4a846b27157e7a0454c143a0ec32
emilyaikens <emily.c.aikens@gmail.com>
2022-12-22 22:43:19 -0800

    add answercard to questionpage. need to change to radio

---
+++ src/components/AnswerCard.jsx
@@ -0,0 +1,12 @@
+
+export default function AnswerCard({ answer }) {
+
+    return (
+        <div>
+            <div>{ answer[0] }</div>
+            <div>{ answer[1] }</div>
+            <div>{ answer[2] }</div>
+            <div>{ answer[3] }</div>
+        </div>
+    )
+};
--- src/pages/Question/QuestionPage.jsx
+++ src/pages/Question/QuestionPage.jsx
@@ -1,15 +1,21 @@
 import './QuestionPage.css';
 import QuestionCard from '../../components/QuestionCard';
+import AnswerCard from '../../components/AnswerCard';
 
 export default function QuestionPage({ quesAns }) {
 
     const questions = quesAns.map((value, index) =>
         <QuestionCard key={index} index={index} question={value.question} />)
 
+    const answers = quesAns.map((value, index) =>
+        <AnswerCard key={index} index={index} answer={value.answer} />)
+
     return (
         <div>
             <h1>Question Page</h1>
             { questions[0] }
+            <br/>
+            { answers[0] }
         </div>
     )
 };
