7e347c430445cc9fe4981d4799e8080688e43aed
emilyaikens <emily.c.aikens@gmail.com>
2022-12-22 18:59:18 -0800

    create data.js and populate with array of objects questions and answers

---
+++ src/data.js
@@ -0,0 +1,119 @@
+const quesAns = [
+    {
+        "question": "q1",
+        "answer": [
+            "a1",
+            "b1",
+            "c1",
+            "d1"
+        ]
+    },
+    {
+        "question": "q2",
+        "answer": [
+            "a2",
+            "b2",
+            "c2",
+            "d2"
+        ]
+    },    
+    {
+        "question": "q3",
+        "answer": [
+            "a3",
+            "b3",
+            "c3",
+            "d3"
+        ]
+    },    
+    {
+        "question": "q4",
+        "answer": [
+            "a4",
+            "b4",
+            "c4",
+            "d4"
+        ]
+    },    
+    {
+        "question": "q5",
+        "answer": [
+            "a5",
+            "b5",
+            "c5",
+            "d5"
+        ]
+    },    
+    {
+        "question": "q6",
+        "answer": [
+            "a6",
+            "b6",
+            "c6",
+            "d6"
+        ]
+    },    
+    {
+        "question": "q7",
+        "answer": [
+            "a7",
+            "b7",
+            "c7",
+            "d7"
+        ]
+    },    
+    {
+        "question": "q8",
+        "answer": [
+            "a8",
+            "b8",
+            "c8",
+            "d8"
+        ]
+    },    
+    {
+        "question": "q9",
+        "answer": [
+            "a9",
+            "b9",
+            "c9",
+            "d9"
+        ]
+    },    
+    {
+        "question": "q10",
+        "answer": [
+            "a10",
+            "b10",
+            "c10",
+            "d10"
+        ]
+    },    
+    {
+        "question": "q11",
+        "answer": [
+            "a11",
+            "b11",
+            "c11",
+            "d11"
+        ]
+    },    
+    {
+        "question": "q1",
+        "answer": [
+            "a1",
+            "b1",
+            "c1",
+            "d1"
+        ]
+    },
+    {
+    "question": "q12",
+    "answer": [
+        "a12",
+        "b12",
+        "c12",
+        "d12"
+        ]
+    },
+]
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import QuestionPage from '../Question/QuestionPage';
+import AnswerPage from '../Answer/AnswerPage';
+import ResultPage from '../Result/ResultPage';
 
 function App() {
+
   return (
     <div>
       <div class="App">
         <h1>Fish Quiz</h1>
+        < QuestionPage />
+        < AnswerPage />
+        < ResultPage />
       </div>
     </div>
   );
