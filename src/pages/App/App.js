92cb22f72e1b8179ca69c0ce1211faf4af04b1f5
emilyaikens <emily.c.aikens@gmail.com>
2022-12-22 22:05:50 -0800

    add use state to app.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,20 +2,30 @@ import './App.css';
 import QuestionPage from '../Question/QuestionPage';
 import AnswerPage from '../Answer/AnswerPage';
 import ResultPage from '../Result/ResultPage';
+import LandingPage from '../Landing/LandingPage';
 import { quesAns } from '../../data';
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+
+  const [quizStatus, setQuizStatus] = useState("landing");
 
   return (
     <div>
       <div class="App">
-        <h1>Fish Quiz</h1>
-        <Routes>
-          < Route path="/question" element={<QuestionPage quesAns={ quesAns } />} />
-          < Route path="/answer" element={<AnswerPage />} />
-          < Route path="/Result" element={<ResultPage />} />
-        </Routes>
+        {quizStatus == "landing"?
+          <>
+            <h1>Fish Quiz</h1>
+            <LandingPage />
+          </>
+          :
+          <Routes>
+            < Route path="/question" element={<QuestionPage quesAns={quesAns} setQuizStatus={setQuizStatus}/>} />
+            < Route path="/answer" element={<AnswerPage />} />
+            < Route path="/result" element={<ResultPage />} />
+          </Routes>
+        }
       </div>
     </div>
   );
--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,13 +1,17 @@
 import './LandingPage.css';
+import { Link } from 'react-router-dom';
+
 
 export default function LandingPage() {
     return (
         <div>
             <h1>Landing Page</h1>
+            <Link to={`/question`}>
+                <button>Start</button>
+            </Link>
         </div>
     )
 };
-
 
 
 
